290e5e2f49df6da44877a96c428ce0eebdc00cd0
Marton Soos <marton.soos@secrethub.io>
2020-02-11 11:49:17 +0100

    Fix config and test

--- test.js
+++ test.js
@@ -1,13 +1,6 @@
-/*assert = require('assert');
+assert = require('assert');
 
 assert(process.env.DB_PASS.length > 9);
 assert(process.env.DB_PASS[1] === 'e');
 assert(process.env.DB_USER.length > 5);
 assert(process.env.DB_HOST.length > 5);
-*/
-
-if(process.env.DB_PASS.length > 9) {
-    console.log("ok")
-} else {
-    console.log("not ok")
-}
